2e0239cb897544e09dd1d82bc4b7edf4c6754154
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-11-23 07:04:42 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,15 +2,22 @@
 
 import React, { useContext } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function NotFound() {
   return (
     <div className={`w-full min-h-screen flex flex-col items-center`}>
 
+      <div className="absolute top-0 flex flex-row justify-between items-center md:w-10/12 w-11/12 h-auto py-6">
+        <Link href={'/'}>
+          <Image src={'/wild-robot.svg'} alt='wild robot logo' width={120} height={50} />
+        </Link>
+      </div>
+
       <div className='w-10/12 h-auto flex flex-col items-center justify-center text-2xl mt-44'>
         <h1 className='text-3xl mb-4'>404 - Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <p>Go back to the <Link href="/" className='font-semibold transition-all hover:underline'>homepage</Link>.</p>
+        <p>Sorry, i think i haven't created this route yet.</p>
+        <p>But we can explore the <Link href="/" className='font-semibold transition-all hover:underline'>homepage</Link> together.</p>
       </div>
 
     </div>
