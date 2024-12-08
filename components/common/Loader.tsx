d30d8e27b29304d5fac07a8b98aeae26d6e4aad3
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-12-08 22:38:54 +0530

    update loader

--- components/common/Loader.tsx
+++ components/common/Loader.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import { Card, CardContent } from '../ui/card'
 
 const Loader = () => {
     return (
         <div className='w-full h-auto flex flex-row flex-wrap gap-6'>
-            {[0, 1, 2, 3, 4, 5].map((id) => (
-                <div key={id} className="md:w-[32%] h-40 w-full relative">
-                    <Card className="h-full text-white bg-roz border-none">
-                        <CardContent className='flex items-center justify-center h-full w-full p-0'>
-                            <div className="loader"></div>
-                        </CardContent>
-                    </Card>
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+                <div className="pyramid-loader">
+                    <div className="wrapper">
+                        <span className="side side1"></span>
+                        <span className="side side2"></span>
+                        <span className="side side3"></span>
+                        <span className="side side4"></span>
+                        <span className="shadow"></span>
+                    </div>
                 </div>
-            ))}
+            </div>
         </div>
     )
 }
