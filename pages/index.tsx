472900e3bc9eaa23a50e86a08e88485901be751c
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-12-19 22:49:33 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,17 +8,17 @@ import { FaGoogle } from "react-icons/fa";
 import { IoPersonCircleOutline } from "react-icons/io5";
 
 export default function Home() {
-  const { admin, userEmail } = useContext(UserContext);
+  const { admin } = useContext(UserContext);
 
   return (
-    <div className="w-full h-auto min-h-screen flex flex-col items-center justify-start py-6 text-white bg-mando">
+    <div className="w-full h-screen flex flex-col items-center justify-start py-6 text-white bg-mando">
 
       <Head>
         <link rel="icon" href="/header.png" type="image/png" sizes="70x70" />
         <title>Roz</title>
       </Head>
 
-      <div className="w-11/12 h-full flex flex-col">
+      <div className="w-11/12 h-full flex flex-col justify-between">
 
         {/* navbar */}
         <div className='h-auto flex flex-row justify-between items-center mt-6 border-2 border-white rounded-full shadow'>
@@ -48,6 +48,17 @@ export default function Home() {
               <FaGoogle className="text-black" />
             </div>
           )}
+        </div>
+
+        <div className="flex flex-col ml-4">
+          <div className="md:text-9xl text-2xl font-bold text-white">Rozzum</div>
+          <div className="text-base font-light ml-1">here is the description</div>
+        </div>
+
+        <div className="flex flex-row gap-6 flex-wrap items-center justify-center">
+          <div className="md:w-[32%] w-full h-52 border-2 border-white bg-black rounded-lg p-6">Feature 1</div>
+          <div className="md:w-[32%] w-full h-52 border-2 border-white bg-black rounded-lg p-6">Feature 2</div>
+          <div className="md:w-[32%] w-full h-52 border-2 border-white bg-black rounded-lg p-6">Feature 3</div>
         </div>
 
       </div>
